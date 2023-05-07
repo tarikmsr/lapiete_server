1f38e6ce9a4ee38acd3e53bb9b70cde57873c8df
Tarik El Msaouri <tarikmsr8@gmail.com>
2023-05-07 16:47:34 +0000

    save test version

--- methods/get-request.js
+++ methods/get-request.js
@@ -1,16 +1,17 @@
 const { pool } = require('../methods/connection');
-// require('dotenv').config();
-// const bcrypt = require('bcryptjs');
-// const jwt = require('jsonwebtoken');
-// const connection = require('../methods/connection');
+const jsonP = require('../util/tojson-parser');
+const jsonUplodedDocs = require('../util/uploaded-docs-parser');
+const fs = require('fs');
+
+
 
 const tablesName =[
   'defunt',
   'decisionnaire',
   'filiation',
+  'situation_familiale',
   'deces',
   'mise_en_biere',
-  'situation_familiale',
   'cimetiere',
   'concession',
   'rapatriement',
@@ -34,7 +35,6 @@ module.exports = async (req, res) => {
   // const regexV4 = new RegExp(
   // /^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i
   // );
-
 
   if (req.url === "/api/user" ) {  //&& regexNumbers.test(id)
 
@@ -78,10 +78,6 @@ module.exports = async (req, res) => {
 
   } else if (!regexNumbers.test(id) && !regexLetters.test(id)) {
 
-    console.log("test url 81 get ");
-    console.log(!regexNumbers.test(id));
-    console.log(!regexLetters.test(id));
-
     res.writeHead(404, { "Content-Type": "application/json" });
     res.end(
       JSON.stringify({
@@ -89,8 +85,69 @@ module.exports = async (req, res) => {
         message: "L'UUID n'est pas valide ou l'itinéraire n'a pas été trouvé",
       })
     );
-  } else if (baseUrl === "/api/form/" && regexNumbers.test(id)) {
+  } else if (baseUrl === "/api/form/docs/" && regexNumbers.test(req.url.split("/")[4]) ) {
 
+    let id = req.url.split("/")[4];
+    await getOneDefuntuploadedDataById(id,'act_naissance') ///cni_fr_defunt
+    .then(data => {    
+      console.log("data-----116");
+      console.log(data);  
+
+      // const blob = new Blob([data], { type: 'application/octet-stream' });
+      // console.log("----------------109-blob---------");
+      // console.log(blob);
+      // console.log("----------------111-blob---------\n\n");
+
+      
+      // res.status(200); 
+      // res.setHeader('Content-Type', 'application/octet-stream');
+      // res.send(blob);
+
+      
+      // const data = await getOneDefuntuploadedDataById(id, 'cni_fr_defunt');
+      const filename = `file_${id}.bin`;
+      res.setHeader('Content-Type', 'application/octet-stream');
+      // res.setHeader('Content-Disposition', `attachment; filename="${filename}"`);
+      res.send(data);
+
+
+      // const filename = `file_${id}.bin`;
+
+      // const url = URL.createObjectURL(blob);
+      // const link = document.createElement('a');
+      // link.href = url;
+      // link.download = filename;
+      // document.body.appendChild(link);
+      // link.click();
+      // document.body.removeChild(link);
+      // res.status(200).end();
+
+
+      
+
+      // res.end(
+        // JSON.stringify({'test':'testv'})
+      // );
+
+      // res.writeHead(200, { "Content-Type": "application/json" });
+      // let jsonResult = JSON.stringify(data);
+      // res.end(jsonResult);
+
+
+       //// res.end(Buffer.from(blob));
+    })
+    .catch(err => {
+      console.log(err)
+
+      res.writeHead(404, { "Content-Type": "application/json" });
+      res.end(
+        JSON.stringify(err)
+      );
+    });
+    
+
+
+  } else if (baseUrl === "/api/form/" && regexNumbers.test(id)) {
 
     //check token
     // console.log(92)
@@ -110,7 +167,7 @@ module.exports = async (req, res) => {
       );
     });
 
-  } else if (baseUrl === "/api/form/" && regexLetters.test(id)) {    
+   } else if (baseUrl === "/api/form/" && regexLetters.test(id)) {    
 
     //check token
     // console.log(109)
@@ -211,6 +268,62 @@ function getAllDefuntsData() {
 
 
 /**
+ * Retrieves a defunt and their uploaded docs associated data by their ID.
+ *
+ * @param {number} numeroDefunt - The ID of the defunt to retrieve.
+ * @returns {Promise<Object>} A Promise that resolves to an object containing the
+ * retrieved data, with keys for each table name and values for the corresponding
+ * row data.
+ * @throws {Error} If there is an error retrieving data from the database.
+ */
+async function getOneDefuntuploadedDataById(numeroDefunt, fileName){
+  return new Promise(async (resolve, reject) => {
+    let connection;
+  try {
+    let data = {};
+     connection = await pool.acquire();
+
+
+     // ${fileName}
+
+     const query = `SELECT *
+     FROM uploaded_documents AS upd WHERE upd.numeroDefunt = ${numeroDefunt}`;
+
+     const [rows, fields] = await connection.execute(query);
+
+     console.log("rows");
+     console.log(rows);
+    //  let bufferFile = Buffer.from(rows[0]['cni_fr_defunt'].map(e => Number(e))).toJSON().data ?? null;
+    // let jsonData = await jsonUplodedDocs(rows);
+    // resolve(rows[0][`${fileName}`]);
+    const blob = new Blob(rows[0]['cni_fr_defunt'], { type: 'application/octet-stream' });
+
+    
+    resolve(blob);
+
+
+     
+  } catch (err) {
+    if (connection) await connection.rollback();
+    console.log(err);
+    reject({
+      error:'Error-retrieving-database',
+      // error: err
+    });
+  } finally {
+    if (connection) {
+      await pool.release(connection);
+    }
+  }
+  });
+
+
+}
+
+
+
+
+/**
  * Retrieves a defunt and their associated data by their ID.
  *
  * @param {number} numeroDefunt - The ID of the defunt to retrieve.
@@ -227,6 +340,59 @@ async function getOneDefuntDataById(numeroDefunt){
     let data = {};
      connection = await pool.acquire();
 
+     const query = `SELECT *
+     FROM defunt AS d
+     LEFT JOIN decisionnaire AS dcr ON d.numeroDefunt = dcr.numeroDefunt
+     LEFT JOIN filiation AS f ON d.numeroDefunt = f.numeroDefunt
+     LEFT JOIN situation_familiale AS sf ON d.numeroDefunt = sf.numeroDefunt
+     LEFT JOIN deces AS dc ON d.numeroDefunt = dc.numeroDefunt
+     LEFT JOIN mise_en_biere AS mb ON d.numeroDefunt = mb.numeroDefunt
+     LEFT JOIN cimetiere AS cm ON d.numeroDefunt = cm.numeroDefunt
+     LEFT JOIN concession AS cc ON d.numeroDefunt = cc.numeroDefunt
+     LEFT JOIN rapatriement AS rp ON d.numeroDefunt = rp.numeroDefunt
+     LEFT JOIN vol AS v ON d.numeroDefunt = v.numeroDefunt
+     WHERE d.numeroDefunt = ${numeroDefunt}`;
+
+     const [rows, fields] = await connection.execute(query);
+
+    //  LEFT JOIN uploaded_documents AS upd ON d.numeroDefunt = upd.numeroDefunt
+    //  LEFT JOIN generated_documents AS gd ON d.numeroDefunt = gd.numeroDefunt
+
+    let jsonData = await jsonP(rows);
+
+    console.log("---------- 307 ---------");
+
+    // let docFile = await getOneDefuntuploadedDataById(numeroDefunt,'cni_fr_defunt');
+
+    // await getOneDefuntuploadedDataById(connection,numeroDefunt,'cni_fr_defunt')
+    // .then(data => {   
+
+    //   console.log("data--------");
+    //   console.log(data);
+          
+    // console.log("---------- 310 ---------");
+
+    //   resolve(data);
+    // })
+    // .catch(err => {
+    //   console.log(err)
+    //   reject({
+    //     error:'Error-retrieving-database',
+    //     msg: err //
+    //   });
+
+    // });
+
+
+
+
+
+    ///
+    resolve(jsonData);
+
+
+
+if(1===2){ //hide for test
     for (let i = 0; i < tablesName.length; i++) {
       const table = tablesName[i];
       const query = `SELECT * FROM ${table} WHERE numeroDefunt = ${numeroDefunt}`;
@@ -251,8 +417,7 @@ async function getOneDefuntDataById(numeroDefunt){
 
         
 
-      } else
-       if(table == tablesName[0]){
+      } else if(table == tablesName[0]){
         reject({
           error:'defunt-doesnot-exist',
         });
@@ -264,9 +429,14 @@ async function getOneDefuntDataById(numeroDefunt){
 
       if (i == tablesName.length - 1) {
         data['error'] = null;
+
+        console.log("-------------4800--------");
+        console.log(data);
         resolve(data);
       }
     } //end for
+
+  }//end id test
 
   } catch (err) {
     if (connection) await connection.rollback();
@@ -304,8 +474,11 @@ async function getDefuntDataByName(lastName) {
       try {
         const [rows, fields] = await connection.execute(query);
         results = rows;
+
         console.log(rows);
         console.log(rows.length == 1);
+
+
         if(rows.length == 1){
           var id = rows[0]['numeroDefunt'];
            if (connection) { //end the first
--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const express = require('express');
 const cors = require('cors');
 
 const PORT = process.env.PORT || 3000;
-
 const app = express();
 
 app.use(cors({
